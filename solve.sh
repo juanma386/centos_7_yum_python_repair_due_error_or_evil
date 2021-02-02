24d068b1e5aaaa5eb747eb90da9425c1b58ef328
Juan manuel Pedro Villalba <xomie_01@hotmail.com>
2021-02-02 20:14:17 -0300

    This is created to help
    This is created to help anyone who comes up with this failure, emergency repair, if you have saved your server reflect your thanks by transferring me to my account PayPal a donation.

---
+++ solve.sh
@@ -0,0 +1,113 @@
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/bzip2-devel-1.0.6-13.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/compat-db-4.7.25-28.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/dlm-devel-4.0.7-1.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/dlm-lib-4.0.7-1.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/gdbm-1.10-8.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/gdbm-devel-1.10-8.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/keyutils-libs-devel-1.5.8-3.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/krb5-devel-1.15.1-46.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/libcom_err-devel-1.42.9-17.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/libdb-devel-5.3.21-25.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/libdb-tcl-5.3.21-25.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/libdb-tcl-devel-5.3.21-25.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/libselinux-devel-2.5-15.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/libsepol-devel-2.5-10.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/libverto-devel-0.2.5-4.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/ncurses-devel-5.9-14.20130511.el7_4.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/openssl-devel-1.0.2k-19.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/pkgconfig-0.27.1-4.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-2.7.5-88.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-devel-2.7.5-88.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-libs-2.7.5-88.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-pycurl-7.19.0-19.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/sqlite-3.7.17-8.el7_7.1.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/sqlite-devel-3.7.17-8.el7_7.1.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/sqlite-tcl-3.7.17-8.el7_7.1.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-setuptools-0.9.8-7.el7.noarch.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-six-1.9.0-2.el7.noarch.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-tools-2.7.5-88.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-urlgrabber-3.10-10.el7.noarch.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/readline-6.2-11.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/readline-devel-6.2-11.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/popt-devel-1.13-16.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-4.11.3-43.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-apidocs-4.11.3-43.el7.noarch.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-build-4.11.3-43.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-build-libs-4.11.3-43.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-devel-4.11.3-43.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpmdevtools-8.3-5.el7.noarch.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-libs-4.11.3-43.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-python-4.11.3-43.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/sqlite-3.7.17-8.el7_7.1.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/sqlite-devel-3.7.17-8.el7_7.1.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/tcl-devel-8.5.13-8.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/tix-8.4.3-12.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/tix-devel-8.4.3-12.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/tk-devel-8.5.13-6.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/tkinter-2.7.5-88.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/yum-3.4.3-167.el7.centos.noarch.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/yum-metadata-parser-1.1.4-10.el7.x86_64.rpm
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/zlib-devel-1.2.7-18.el7.x86_64.rpm 
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/krb5-devel-1.15.1-50.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/libcom_err-devel-1.42.9-19.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-2.7.5-89.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-devel-2.7.5-89.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/python-libs-2.7.5-89.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-4.11.3-45.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-apidocs-4.11.3-45.el7.noarch.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-build-4.11.3-45.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-build-libs-4.11.3-45.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-devel-4.11.3-45.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpmdevtools-8.3-7.el7.noarch.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-libs-4.11.3-45.el7.x86_64.rpm	
+wget	http://mirror.centos.org/centos/7/os/x86_64/Packages/rpm-python-4.11.3-45.el7.x86_64.rpm	
+rm *.1 *.2
+rpm -Uvh --replacepkgs --force --nodeps	bzip2-devel-1.0.6-13.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	compat-db-4.7.25-28.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	dlm-devel-4.0.7-1.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	dlm-lib-4.0.7-1.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	gdbm-1.10-8.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	gdbm-devel-1.10-8.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	keyutils-libs-devel-1.5.8-3.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	krb5-devel-1.15.1-50.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	libcom_err-devel-1.42.9-19.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	libdb-devel-5.3.21-25.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	libdb-tcl-5.3.21-25.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	libdb-tcl-devel-5.3.21-25.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	libselinux-devel-2.5-15.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	libsepol-devel-2.5-10.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	libverto-devel-0.2.5-4.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	ncurses-devel-5.9-14.20130511.el7_4.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	openssl-devel-1.0.2k-19.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	pkgconfig-0.27.1-4.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	popt-devel-1.13-16.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	python-2.7.5-89.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	python-devel-2.7.5-89.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	python-libs-2.7.5-89.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	python-pycurl-7.19.0-19.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	python-setuptools-0.9.8-7.el7.noarch.rpm
+rpm -Uvh --replacepkgs --force --nodeps	python-six-1.9.0-2.el7.noarch.rpm
+rpm -Uvh --replacepkgs --force --nodeps	python-urlgrabber-3.10-10.el7.noarch.rpm
+rpm -Uvh --replacepkgs --force --nodeps	readline-6.2-11.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	readline-devel-6.2-11.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	rpm-4.11.3-45.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	rpm-apidocs-4.11.3-45.el7.noarch.rpm
+rpm -Uvh --replacepkgs --force --nodeps	rpm-build-4.11.3-45.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	rpm-build-libs-4.11.3-45.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	rpm-devel-4.11.3-45.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	rpmdevtools-8.3-7.el7.noarch.rpm
+rpm -Uvh --replacepkgs --force --nodeps	rpm-libs-4.11.3-45.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	rpm-python-4.11.3-45.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	sqlite-3.7.17-8.el7_7.1.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	sqlite-devel-3.7.17-8.el7_7.1.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	sqlite-tcl-3.7.17-8.el7_7.1.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	tcl-devel-8.5.13-8.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	tix-8.4.3-12.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	tix-devel-8.4.3-12.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	tk-devel-8.5.13-6.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	yum-metadata-parser-1.1.4-10.el7.x86_64.rpm
+rpm -Uvh --replacepkgs --force --nodeps	zlib-devel-1.2.7-18.el7.x86_64.rpm
+
+yum update -y
+
+echo 'EN: If you've come this far, I hope it solves the problem for you, remember that if I've helped you, you can thank me with a donation, ES: Si has llegado hasta aquí espero que te solucione el problema, recuerda que si te eh ayudado puedes agradecerme con una donacion,JP: あなたがここまで来たなら、私はそれがあなたのために問題を解決することを願っています、私があなたを助けたなら、あなたは寄付で私に感謝できることを覚えておいてください、, BR:Se você chegou até aqui, espero que resolva o problema para você, lembre-se que se eu te ajudei, você pode me agradecer com uma doação,RU:Если вы так далеко, я надеюсь, что это решит проблему для вас, помните, что если я помог вам, вы можете поблагодарить меня с пожертвованием,DE:Wenn Sie so weit gekommen sind, hoffe ich, dass es das Problem für Sie löst, denken Sie daran, dass, wenn ich Ihnen geholfen habe, Können Sie mir mit einer Spende danken,AR:إذا كنت قد وصلت إلى هذا الحد، وآمل أن يحل المشكلة بالنسبة لك، تذكر أنه إذا كنت قد ساعدتك، يمكنك أن تشكرني مع التبرع،CH:如果你已经到这一步，我希望它为你解决问题，记住，如果我帮助你，你可以感谢我捐赠，CH:如果你已經到這一步，我希望它為你解決問題，記住，如果我説明你，你可以感謝我捐贈，SL:Ak ste zašli tak ďaleko, dúfam, že to vyrieši problém za vás, pamätajte, že ak som vám pomohol, môžete mi poďakovať darom,SLV: Če ste prišli tako daleč, upam, da bo rešil problem za vas, ne pozabite, da če sem vam pomagal, se mi lahko zahvalite z donacijo,FR:Si vous êtes venu aussi loin, j’espère que ça résout le problème pour vous, rappelez-vous que si je vous ai aidé, vous pouvez me remercier avec un don,H:אם התתת עד לכאן, אני מקווה שזה יפתור את הבעיה בשבילך, תזכור שאם עזרתי לך, אתה יכול להודות לי בתרומה,'
